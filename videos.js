f08efe5f235bf36f452e78d55bcc68e768beda34
Kabian Davidson <kabiandavidson@Kabians-MacBook-Pro.local>
2023-11-29 12:10:30 -0500

    Initial commit

--- videos.js
+++ videos.js
@@ -1,7 +1,7 @@
 // This file contains the array of video links
 // Add or modify video links as needed
 const videoLinks = [
-  "https://www.youtube.com/watch?v=_LrOzWu5lKU",
+  "https://youtu.be/hSp4zEbL1VM?si=GNYzPVwsQrDzd7Nf",
   "https://www.youtube.com/watch?v=4bGh1DEpMKI",
   // Add more video links as needed
 ];
